de7447ce0481943f201695ebaa0e6802f21905ad
Mart M <repogfreow@gmail.com>
2016-03-06 21:06:02 +0200

    javascript defer

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 Mart Mägi, Harry Saar, Vitaly Kuzmin
 
 [Testkeskkond](https://cryptic-ravine-26647.herokuapp.com)
+
+[Projektiplaan](https://github.com/vtlkzmn/VeebirakendusteLoomine/wiki)
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <title>Veebirakenduste projekt</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" defer></script>
     <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/css/style.css">
