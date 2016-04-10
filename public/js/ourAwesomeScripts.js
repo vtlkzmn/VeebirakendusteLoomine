f2307fd69ae871f1900e5901e03dd9482418b137
saaruniversity <saaruniversity@gmail.com>
2016-04-10 18:15:07 +0300

    offline functionality

--- public/js/ourAwesomeScripts.js
+++ public/js/ourAwesomeScripts.js
@@ -8,6 +8,8 @@ function bonusButton() {
 
 
 
+
+
 /* data push */
 function poll(){
 
@@ -27,7 +29,86 @@ function poll(){
 
 }
 
+/* kontrollime kas on midagi lisada andmebaasi */
+
+function addToDatabase(a_last, d_last){
+	// ajaxiga või millegi muuga peaks selle nüüd saatma 
+	$.ajax({
+        type: "POST",
+        url: "/estates/addEstate",
+        data: "subject="+a_last+"&body="+d_last, 
+        success: function(data)
+        {
+            alert("Edukalt lisatud andmebaasi storage olnud failid!"); 
+        },
+        error: function(data){
+        	// online method ei ole väga hea..
+        	// seega peab kordama, kui ebaõnnestus
+        	setTimeout(function(){
+		   		addToDatabase(a_last,d_last);
+		 	}, 10000); 
+        }
+    });
+}
+
+function kontrolliStorage(){
+
+	if(window.navigator.onLine){
+		var a_last = localStorage.address_offline_last;
+		var d_last = localStorage.description_offline_last;
+		if(typeof a_last === "undefined" || typeof d_last === "undefined"){
+		// empty storage
+		}
+		else{
+			
+			addToDatabase(a_last,d_last);
+			// eemaldame storge-st
+			localStorage.removeItem("address_offline_last");
+			localStorage.removeItem("description_offline_last");						
+		}
+	}
+	else{
+		// ootame veel, millal online tuleb
+		setTimeout(function(){
+		   	kontrolliStorage();
+		}, 10000); 
+	}	
+}
+
+kontrolliStorage();
+
+window.addEventListener("online", function(e) {
+	kontrolliStorage();
+}, false);
+
 $(window).load(function() {
+
+	/* võrguühenduseta funktsionaalus  */
+	if(document.getElementById("addEstate-button") != null){
+
+		$("#addEstate-button").click(function(event)
+		{
+			if(window.navigator.onLine){
+				var a = document.getElementById("address-offline").value;
+				var d = document.getElementById("description-offline").value;
+				if(a == "" || b ==  ""){
+					alert("Pead täitma väljad!");
+					event.preventDefault();
+				}
+			}
+			else{
+				event.preventDefault(); 
+				var a = document.getElementById("address-offline").value;
+				var d = document.getElementById("description-offline").value;
+				localStorage.setItem("address_offline_last",a);
+				localStorage.setItem("description_offline_last",d);
+				// muudame tühjaks, et tunduks, et midagi lahedat toimus!
+				document.getElementById("address-offline").value = "";
+				document.getElementById("description-offline").value = "";				
+				alert("Salvestasime aadressi:\""+a + "\" ja kirjelduse: \"" + d + "\"\nKui interneti ühenduse tuleb tagasi, siis see lisatakse!");
+			}
+		});
+	}
 
 	fillXML();
 
